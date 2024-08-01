032a42ec42e6a88a7a56a740e6612f54f97c37b2
MOONALIA\Moon <mariefrance.fritz@outlook.fr>
2024-08-01 10:39:14 +0200

    add conditionnal content

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -5,8 +5,8 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({required: true}) name!: string;
+  @Input() name?: string;
 }
