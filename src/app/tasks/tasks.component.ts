9b72767f3f176e7e2ecd9b7bad02c7b6e2e01e62
MOONALIA\Moon <mariefrance.fritz@outlook.fr>
2024-08-02 10:11:51 +0200

    adding task to users

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -11,6 +11,7 @@ import { TaskComponent } from './task/task.component';
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
   tasks = [
     {
       id: 't1',
@@ -37,7 +38,7 @@ export class TasksComponent {
     },
   ];
 
-  get selectedUserTasks(){
+  get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
-  };
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// };
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -19,8 +8,7 @@ interface User {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) user!: User ;
-
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
