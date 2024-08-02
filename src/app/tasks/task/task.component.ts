f49d728de3f8dccacea3f4b29558823e9be2dc4e
MOONALIA\Moon <mariefrance.fritz@outlook.fr>
2024-08-02 11:09:59 +0200

    Add new task form

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,17 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
+import { NewTaskComponent } from '../new-task/new-task.component';
 
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [NewTaskComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete= new EventEmitter<string>();
+  @Output() complete = new EventEmitter<string>();
 
   onCompleteTask(){
     this.complete.emit(this.task.id);
