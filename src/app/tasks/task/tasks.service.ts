5501d7262c1ef2cbb5fd8e1e6c614da8f9b56d74
MOONALIA\Moon <mariefrance.fritz@outlook.fr>
2024-08-21 11:09:04 +0200

    finish details of app

--- src/app/tasks/task/tasks.service.ts
+++ src/app/tasks/task/tasks.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import type { NewTaskData } from "./task.model";
+import { Injectable } from '@angular/core';
+import type { NewTaskData } from './task.model';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
@@ -29,11 +29,19 @@ export class TasksService {
     },
   ];
 
-  getUserTasks(userId: string){
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
+  getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
 
-  addTask(taskData: NewTaskData, userId: string){
+  addTask(taskData: NewTaskData, userId: string) {
     this.tasks.unshift({
       id: new Date().getTime().toString(),
       userId: userId,
@@ -41,9 +49,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
-  removeTask(id: string){
+  removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
